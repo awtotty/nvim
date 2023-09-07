da0c0e4e5e58c23bf25452e04916fb26f87c5f60
Austin <awtotty@gmail.com>
2023-09-06 20:18:53 -0700

    Diagnostics virtual text toggle

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,29 +1,28 @@
 local lsp = require('lsp-zero')
 
 lsp.preset({
-  manage_nvim_cmp = {
-    set_sources = 'recommended'
-  }
+    manage_nvim_cmp = {
+        set_sources = 'recommended'
+    }
 })
 
 -- turn off warning and error signs on left gutter
 lsp.set_sign_icons({
-	error = " ",
-	warn = " ",
-	hint = " ",
-	info = " ",
+    error = '✘',
+    warn = '▲',
+    hint = '⚑',
+    info = '»'
 })
 
 lsp.on_attach(function(client, bufnr)
     -- lsp.default_keymaps({buffer = bufnr})
-    local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     -- commenting out to not conflict with harpoon nav
     -- vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-
 end)
 
 lsp.setup()
@@ -35,10 +34,14 @@ local cmp_action = require("lsp-zero").cmp_action()
 cmp.setup({
     mapping = {
         -- `Enter` key to confirm completion
-        ['<CR>'] = cmp.mapping.confirm({select = true}),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
         -- Ctrl+Space to trigger completion menu
         ['<C-Space>'] = cmp.mapping.complete(),
     }
 })
 
+-- turn off "virtual text" in diagnostics
+-- vim.diagnostic.config({
+--     virtual_text = false,
+-- })
