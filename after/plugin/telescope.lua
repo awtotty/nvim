da67bf4a06a186af016113a6701a1c8f36d6fb74
Austin Totty <awtotty@gmail.com>
2025-07-01 21:30:18 -0700

    current direct file create and delete

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -10,17 +10,25 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
--- create a new file
-vim.keymap.set('n', '<leader>pn', function()
-    local filename = vim.fn.input("New file: ")
+function create_file(filename)
     if filename ~= "" then
         vim.cmd("edit " .. filename)
     end
+end
+
+-- create a new file
+vim.keymap.set('n', '<leader>pnf', function()
+    local filename = vim.fn.input("New file: ")
+    create_file(filename)
 end)
 
--- delete a file
-vim.keymap.set('n', '<leader>pd', function()
-    local filename = vim.fn.input("Delete file: ")
+-- create a new file in current directory
+vim.keymap.set('n', '<leader>pnc', function()
+    local filename = vim.fn.input("New file: " .. vim.fn.getcwd() .. "/")
+    create_file(filename)
+end)
+
+function delete_file(filename)
     if filename ~= "" then
         -- Check if the file exists before trying to delete it
         if vim.fn.filereadable(filename) ~= 1 then
@@ -35,4 +43,16 @@ vim.keymap.set('n', '<leader>pd', function()
             print("Deleted file: " .. filename)
         end
     end
+end
+
+-- delete a file
+vim.keymap.set('n', '<leader>pdf', function()
+    local filename = vim.fn.input("Delete file: ")
+    delete_file(filename)
+end)
+
+-- delete a file in current directory
+vim.keymap.set('n', '<leader>pdc', function()
+    local filename = vim.fn.input("Delete file: " .. vim.fn.getcwd() .. "/")
+    delete_file(filename)
 end)
