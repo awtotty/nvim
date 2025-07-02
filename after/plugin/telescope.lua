b2d13356a3aa05fe49f28f35f057fd43a8cc01c7
Austin Totty <awtotty@gmail.com>
2025-07-01 21:06:40 -0700

    File create and delete in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,5 +7,32 @@ vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 
 -- "project search" requires brew install ripgrep on Mac 
 vim.keymap.set('n', '<leader>ps', function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ") }); 
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end)
+
+-- create a new file
+vim.keymap.set('n', '<leader>pn', function()
+    local filename = vim.fn.input("New file: ")
+    if filename ~= "" then
+        vim.cmd("edit " .. filename)
+    end
+end)
+
+-- delete a file
+vim.keymap.set('n', '<leader>pd', function()
+    local filename = vim.fn.input("Delete file: ")
+    if filename ~= "" then
+        -- Check if the file exists before trying to delete it
+        if vim.fn.filereadable(filename) ~= 1 then
+            print("File does not exist: " .. filename)
+            return
+        end
+        local confirm = vim.fn.input("Are you sure you want to delete " .. filename .. "? (y/N) ")
+        if confirm:lower() ~= 'y' then
+            print("Deletion cancelled.")
+        else
+            vim.cmd("silent !rm " .. filename)
+            print("Deleted file: " .. filename)
+        end
+    end
 end)
--- lua/awt/set.lua
+++ lua/awt/set.lua
@@ -4,12 +4,12 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 -- word wrap
-vim.opt.textwidth = 0
-vim.opt.wrapmargin = 0
-vim.opt.wrap = true
--- (optional - breaks by word rather than character): 
-vim.opt.linebreak = true
-vim.opt.columns = 80
+-- vim.opt.textwidth = 0
+-- vim.opt.wrapmargin = 0
+-- vim.opt.wrap = true
+-- -- (optional - breaks by word rather than character): 
+-- vim.opt.linebreak = true
+-- vim.opt.columns = 80
 
 -- tabs and spaces
 vim.opt.tabstop = 2
